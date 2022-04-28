dc2cccd92c722d7e82d0942708b644d2ab8516cb
yugsify <yugsify@gmail.com>
2022-04-29 01:03:12 +0530

    input box is ready!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,9 @@ export default function Header() {
         <header className='header'>
             <div ><h2>Weather App</h2></div>
         </header>
+        <div className='search'>
+            <input type="text" placeholder='Enter City Name..' /><button>Search</button>
+        </div>
     </>
   )
 }
