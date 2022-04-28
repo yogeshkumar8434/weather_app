3b25a422e4bde9a9cf6f4b3891b4e50d1b681803
yugsify <yugsify@gmail.com>
2022-04-29 00:45:43 +0530

    Header Part Complate

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <>
+        <header className='header'>
+            <div ><h2>Weather App</h2></div>
+        </header>
+    </>
+  )
+}
